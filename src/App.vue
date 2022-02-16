b310fe5cba6abc897b7c17a23f3f882a2b9f7d16
Dwicky Candra Lesmana <dwickycandralesmana@gmail.com>
2022-02-17 00:02:41 +0800

    add editor

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,43 @@
 <template>
+	<div class="flex w-screen h-screen text-gray-700">
+		<div class="flex flex-col flex-shrink-0 w-64 border-r border-gray-300 bg-gray-100">
 
+		</div>
+		<div class="flex flex-col flex-grow">
+			<editor-content :editor="editor"/>			
+		</div>
+	</div>
 </template>
 
 <script>
+import { Editor, EditorContent } from '@tiptap/vue-3'
+import StarterKit from '@tiptap/starter-kit'
 
-export default {
-  name: 'App',
-}
+	export default {
+		name: 'App',
+		components: {
+			EditorContent
+		},
+		data() {
+			return {
+				editor: null,
+			}
+		},
+		mounted() {
+			this.editor = new Editor({
+				content: '',
+				extensions: [
+					StarterKit
+				],
+				editorProps: {
+					attributes: {
+						class: "prose my-6 mx-auto focus:outline-none"
+					}
+				}
+			})
+		},
+		beforeUnmount() {
+			this.editor.destroy();
+		}
+	}
 </script>
-
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,5 +7,7 @@ module.exports = {
     variants: {
         extends: {}
     },
-    plugins: []
+    plugins: [
+        require('@tailwindcss/typography')
+    ]
 }
