018263c08ce519a9b6fc6a8b1af6e691dcab7f81
Dwicky Candra Lesmana <dwickycandralesmana@gmail.com>
2022-02-19 22:32:04 +0800

    init indexedDB

--- src/App.vue
+++ src/App.vue
@@ -20,8 +20,12 @@ import StarterKit from '@tiptap/starter-kit'
 		},
 		data() {
 			return {
-				editor: null,
+				editor  : null,
+				database: null,
 			}
+		},
+		async created() {
+            this.database = this.getDatabase();
 		},
 		mounted() {
 			this.editor = new Editor({
@@ -38,6 +42,27 @@ import StarterKit from '@tiptap/starter-kit'
 		},
 		beforeUnmount() {
 			this.editor.destroy();
+		},
+		methods: {
+			async getDatabase() {
+				return new Promise((resolve, reject) => {
+					let db = window.indexedDB.open("notes");
+
+					db.onerror = e => {
+						reject('Error opening the database.');
+					};
+
+					db.onsuccess = e => {
+                        console.log('db.onsuccess', e);
+						resolve(e.target.result);
+					};
+
+					db.onupgradeneeded = e => {
+                        console.log('db.onupgradeneeded', e);
+						e.target.result.createObjectStore("notes");
+					};
+				})
+			}
 		}
 	}
 </script>
